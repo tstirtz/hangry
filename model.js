4dce465178c46209e3658ef3879ef8f06c875b96
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-03-03 11:25:16 -0800

    create and export Restaurant model

--- model.js
+++ model.js
@@ -3,3 +3,19 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(DATABASE_URL);
+
+const restaurantSchema = mongoose.Schema ({
+    userName: String,
+    password: String,
+    list: [
+        {
+            name: String,
+            address: String
+        }
+    ]
+
+});
+
+const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+
+module.exports = {Restaurant};
