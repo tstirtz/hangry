c5f0c61e9bfaa6850871c1a42d88f3bb47d3cc6b
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-03-03 16:23:36 -0800

    adjust position of a constant

--- test/test-restaurant-list-integration.js
+++ test/test-restaurant-list-integration.js
@@ -2,14 +2,13 @@
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const expect = chai.expect;
 const faker = require('faker');
 const mongoose = require('mongoose');
 const {app, runServer, closeServer} = require('../server');
 const {TEST_DATABASE_URL} = require('../config');
 const {Restaurant} = require('../model');
 
-
+const expect = chai.expect;
 chai.use(chaiHttp);
 
 function seedTestData(){
