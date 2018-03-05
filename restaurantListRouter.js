9b50de02ee286f3a2332e742f93e38b855c72717
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-03-04 19:36:14 -0800

    install and require body-parser and create POST endpoint for creating a new user

--- restaurantListRouter.js
+++ restaurantListRouter.js
@@ -1,17 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
 const {DATABASE_URL, PORT} = require('./config');
 const {Users} = require('./models/user-model');
+
+const jsonParser = bodyParser.json();
 
 router.get('/', function(req, res){
     Users
         .find()
         .then(function(items){
+            console.log(items);
             res.json(items.map(item => item.userData()));
         })
         .catch(err => {
             console.log(err);
             res.status(500).json({error: "Something went terribly wrong"})
+        });
+});
+
+//endpoint for creating a new account
+router.post('/', jsonParser, function(req, res){
+    console.log(req.body);
+    const requiredFields = ['userName', 'password'];
+    for(let i = 0; i < requiredFields.length; i++){
+        if(!(requiredFields[i] in req.body)){
+            const message = `Please input ${requiredFields}`;
+            res.status(400).send(message);
+        }
+    }
+
+    Users
+        .create({
+            userName: req.body.userName,
+            password: req.body.password,
+            restaurants:[]
+
+        })
+        .then(function(restaurant){
+            res.status(201).json(restaurant);
+        })
+        .catch(function(err){
+            console.log(err);
+            res.status(500).send({error: "Something went wrong"});
         });
 });
 
