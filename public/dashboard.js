6334d635f70d7aad8a042af5411bebc4c55fda35
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-03-11 11:51:01 -0700

    Make ajax PUT query to add new restaurant to API

--- public/dashboard.js
+++ public/dashboard.js
@@ -51,7 +51,45 @@ function getAndDisplayRandomRestaurant(){
     getRandomRestaurant(renderRandomRestaurant);
 }
 
+function renderRestaurantAddressInput(){
+    $('.add-restaurant-input-js').on('click', function(){
+        $(this).attr('placeholder', 'Restaurant Name');
+        const addressInput = $(this).next();
+        if(addressInput.attr('type') === 'hidden'){
+            addressInput.attr('type', 'text');
+        }
+    });
+}
+
+function sendNewRestaurantData(){
+    $('.add-restaurant-button-js').on('click', function(event){
+        event.preventDefault();
+        console.log(event);
+        let restaurantName = event.target.form[0].value;
+        let address = event.target.form[1].value;
+
+        let restObj = {
+            name: restaurantName,
+            address: address
+        };
+
+        $.ajax({
+            url:'/dashboard/restaurants/5a9c8ec5f037d2fbe9405cb6',
+            method: 'PUT',
+            data: JSON.stringify(restObj),
+            dataType: 'json',
+            contentType: 'application/json; charset= utf-8',
+            success: function(message){
+                console.log(message);
+            }
+        });
+    });
+    //Will add callback to append new restaurant to rendered list of restaurants
+}
+
 $(function(){
     getAndDisplayRestaurants();
     getAndDisplayRandomRestaurant();
+    renderRestaurantAddressInput();
+    sendNewRestaurantData();
 })
