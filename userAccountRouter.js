64bd1e92cd569c0619624f7ce04c899bf12a0e21
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-03-11 17:43:55 -0700

    Update POST endpoint with access control

--- userAccountRouter.js
+++ userAccountRouter.js
@@ -13,24 +13,99 @@ router.post('/', jsonParser, function(req, res){
     const requiredFields = ['userName', 'password'];
     for(let i = 0; i < requiredFields.length; i++){
         if(!(requiredFields[i] in req.body)){
-            const message = `Please input ${requiredFields}`;
-            res.status(400).send(message);
+            return res.status(422).json({
+                code: 422,
+                reason: 'ValidationError',
+                message: 'Missing field',
+                location: requiredFields[i]
+            });
         }
     }
 
-    Users
-        .create({
-            userName: req.body.userName,
-            password: req.body.password
-            // restaurants:[]
+    const fieldsToTrim = ['userName', 'password'];
+    const nonTrimmedFields = fieldsToTrim.find(function(field){
+        req.body[field].trim() !== req.body[field]
+    });
 
+    if(nonTrimmedFields){
+        return res.status(422).json({
+            code: 422,
+            reason: 'ValidationError',
+            message: 'Cannot start or end with whitespace',
+            location: nonTrimmedField
+        });
+    }
+
+    //check for correct length of userName and password
+
+    const sizedFields = {
+        userName: {
+            min: 1
+        },
+        password: {
+            min: 10,
+            max: 72
+        }
+    };
+
+    const tooSmallField = sizedFields.find(function(field){
+        req.body[field].trim().length < sizedFields[field].min
+    });
+
+    const tooLargeField = sizedFields.find(function(field){
+        'max' in sizedFields[field] && req.body[field].trim().length > sizedFields[field].max
+    });
+
+    if(tooSmallField){
+        return res.status(422).json({
+            code: 422,
+            reason: 'ValidationError',
+            message: `Password must be at least ${sizedFields[tooSmallField].min} characters
+            long`,
+            location: tooSmallField
+        });
+    }else if(tooLargeField){
+        return res.status(422).json({
+            code: 422,
+            reason: 'ValidationError',
+            message: `Password must not be more than ${sizedFields[tooLargeField].max} characters
+            long`,
+            location: tooLargeField
+        })
+    }
+
+    return Users
+        .find({userName})
+        .count()
+        .then(function(count){
+            if(count > 0){
+                return Promise.reject({
+                    code: 422,
+                    reason: 'ValidationError',
+                    message: 'Username already taken',
+                    location: 'userName'
+                });
+            }
+            //hashPassword is a static function defined on the userSchema which
+            //uses bcryptjs to hash the password
+            return User.hashPassword(req.body.password);
+        };)
+        .then(function(hash){
+            return Users
+                .create({
+                    userName: req.body.userName,
+                    password: hash
+                });
         })
         .then(function(user){
-            res.status(201).json(user);
+            res.status(201).json(user.userData());
         })
         .catch(function(err){
             console.log(err);
-            res.status(500).send({error: "Something went wrong"});
+            if(err === 'ValidationError'){
+                res.status(err.code).json(err);
+            }
+            res.status(500).send({code: 500, message: "Internal server error"});
         });
 });
 
