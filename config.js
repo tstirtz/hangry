c49f1369f76aa729921b68b3242741cfca241290
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-03-11 17:40:28 -0700

    Create new environment variable

--- config.js
+++ config.js
@@ -3,3 +3,5 @@ exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongo
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || global.TEST_DATABASE_URL || 'mongodb://localhost/hangry-test';
 
 exports.PORT = process.env.PORT || 8080
+
+exports.JWT_SECRET = process.env.JWT_SECRET;
