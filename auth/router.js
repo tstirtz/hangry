fc01154c08accdae4810d05c6203f4d48b3baf0b
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-03-13 19:49:25 -0700

    Create login endpoint

--- auth/router.js
+++ auth/router.js
@@ -4,3 +4,35 @@ const express = require('express');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
+const {Users} = require('../models/user-model')
+
+const config = require('../config');
+const router = express.Router();
+
+
+const createAuthToken = function(user){
+        return jwt.sign({user}, config.JWT_SECRET, {
+            subject: user.userName,
+            expiresIn: config.JWT_EXPIRY,
+            algorithm: 'HS256'
+        });
+}
+
+
+const localAuth = passport.authenticate('local', {session: false});
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.post('/', localAuth, function(req, res){
+    console.log(req.user);
+    const authToken = createAuthToken(req.user.forAuthToken());
+    //createAuthToken needs the username and password passed to it
+    console.log(req.user);
+    res.redirect('/dashboard');
+    // res.json({authToken});
+});
+
+//need to figure out where this authToken is attached to a request in order to
+//properly set up tests
+
+module.exports = {router};
