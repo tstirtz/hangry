6ef28ec7cf4bf46cf5be174cd8f01b95ee2fe65c
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-03-13 19:52:22 -0700

    Update test JWT_SECRET

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || global.TEST_DATABAS
 
 exports.PORT = process.env.PORT || 8080
 
-exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_SECRET = process.env.JWT_SECRET || 'testsecretkey';
 
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
