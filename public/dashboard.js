c6a7ab684aa946672dcf07978b2d1b60d1705af6
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-03-31 22:44:27 -0700

    Add comments and fix tablet typo.

--- public/dashboard.js
+++ public/dashboard.js
@@ -42,7 +42,7 @@ function getRestaurantData(callback){
 
         if($('.restaurant-list-js').hasClass('hide')){
             smallDeviceMediaQuery();
-            tableMediaQuery(); //slide image and generate restaurant button to the right
+            tabletMediaQuery(); //slide image and generate restaurant button to the right
         //if restaurant list isn't hidden then make request to get list of restaurants
             $.ajax({
                 url:'/dashboard/restaurants/' + sessionStorage.getItem('userId'),
@@ -56,7 +56,7 @@ function getRestaurantData(callback){
             });
         }else if(!($('.restaurant-list-js').hasClass('hide'))){
             smallDeviceMediaQuery();
-            tableMediaQuery(); //slide image and generate restaurant button to the right
+            tabletMediaQuery(); //slide image and generate restaurant button to the right
             $('.restaurant-list-js').toggleClass('hide');
         }
     })
@@ -189,7 +189,7 @@ function editRestaurant(){
 function hideEditModal(){
     $('#edit-restaurant-modal').css("display", "none");
     smallDeviceMediaQuery();
-    tableMediaQuery();
+    tabletMediaQuery();
 }
 
 function hideRestaurantList(){
@@ -366,7 +366,7 @@ function deleteRestaurant(restIdToDelete){
 function hideDeleteModal(){
     $('#delete-restaurant-modal').css("display", "none");
     smallDeviceMediaQuery();
-    tableMediaQuery();
+    tabletMediaQuery();
 }
 
 function closeEditModal(){
@@ -407,13 +407,12 @@ function smallDeviceMediaQuery(){
     }
 }
 
-function tableMediaQuery(){
+function tabletMediaQuery(){
     console.log("tableMedia Query started");
     const screenSize = window.matchMedia('(min-width: 611px)');
     if(screenSize.matches){
         $('.generate-restaurant').toggleClass('tablet');
         $('img').toggleClass('tablet');
-
     }
 }
 
